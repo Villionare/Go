43d6664470cbb0045ab0e396e3db9747759a01f6
Stealth <mr.duedull@gmail.com>
2025-07-04 22:13:58 +0530

    Playing with vars

--- helloworld.go
+++ helloworld.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+var new string = "pom";
+
 func main() {
-	fmt.Println("hello this is my first GOOOOOO Program")
+	// var student1 string = "abhay";
+	// student2 := "om";
+	// var student3 = "Jane" //type is inferred
+
+	// fmt.Println(student1);
+	// fmt.Println(student2);
+	// fmt.Println(student3);
+
+	var first string;
+	var second int;
+	var third bool;
+	var fourth float32;
+
+	fmt.Println(first);
+	fmt.Println(second);
+	fmt.Println(third);
+	fmt.Println(fourth);
 }
+
+//go mod init example.com/hello
+//go run .\helloworld.go     
